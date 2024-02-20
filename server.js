ec3cad67911ad1afe56a6bfe6efee8458f36aa32
raj-dev143 <rajendra.meena.dev@gmail.com>
2024-02-20 18:24:09 +0530

    ser

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const apiData = require("./data.json");
 
